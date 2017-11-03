6800833fd38ad9dcd54c2aaf1a7e75619a974e3b
Antoine Mayerowitz <a.mayerowitz@gmail.com>
2017-11-03 14:15:20 +0100

    Add optional features

--- R/DBson.R
+++ R/DBson.R
@@ -1,15 +1,23 @@
 #' DBson
 #'
-#' This function allows you to easily download data formated in json from DBnomics and translate it to data frame
-#' @param "slug" The slug provided on dbnomics
-#' @keywords json
+#' This function allows you to easily download data formated in json from DBnomics and converts it to data frame
+#'
+#' @param "slug" A \emph{string}. The slug provided on dbnomics.
+#' @param frequency A \emph{boolean}. If you want a frequency column in your data frame.
+#' @param source A \emph{boolean}. If you want a data provider column in your data frame.
+#'
+#' @return A \emph{long} data frame with at least 3 columns: \code{time values var}
+#'
 #' @export
-#' @import jsonlite dplyr
+#' @import jsonlite
+#' @import dplyr
 #' @examples
 #' # Download USA GDP from OECD's Economic Outlook
 #' DBson("oecd-eo-usa-gdp-q")
+#'
+#' @references \url{https://db.nomics.world/}
 
-DBson <- function(slug){
+DBson <- function(slug, frequency=TRUE, source = FALSE){
   api <- "https://api.db.nomics.world/api/v1/json/series/"
 
   data <- fromJSON(paste0(api,slug), flatten=TRUE)
@@ -17,6 +25,13 @@ DBson <- function(slug){
     transmute(time = period,
               values = value) %>%
     mutate(var = data[["data"]][["name"]])
+
+  if(frequency){
+    db <- mutate(db, freq = data[["data"]][["frequency"]])
+  }
+  if(source){
+    db <- mutate(db, provider = data[["data"]][["provider_name"]])
+  }
 
   return(db)
 }
